f4b04289aeeb266256e8c4b3374ace110e18a8df
Byron Guallasamin <bguallasamin1999@outlook.com>
2025-05-13 21:33:12 -0500

    Prueba

---
+++ Utilitario/Constante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace BilleteraDigital.Utilitario
+{
+    public static class Constante
+    {
+        public static string DataBasePath {
+            get
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return System.IO.Path.Combine(path, "DB_FINANZAS.db");
+            }
+        }
+        public static SQLiteOpenFlags Flags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+    }
+}
