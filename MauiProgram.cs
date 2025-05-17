6a953592493f953c1b49acce2304f81809fd4999
Mauricio8 <Mauricio8@MAURICIO-DELL>
2025-05-16 23:27:24 -0500

    MAURICIO CREACION DE LOGIN

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace BilleteraDigital
 {
@@ -9,10 +10,12 @@ namespace BilleteraDigital
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("FontAwesomeSolid.otf", "AwesomeSolid");
                 });
 
 #if DEBUG
