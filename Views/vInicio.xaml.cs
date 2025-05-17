dc35b34e7381330c264e1432682e96e9be9f3b12
Mauricio8 <Mauricio8@MAURICIO-DELL>
2025-05-17 10:31:32 -0500

    Mauricio Login 3

--- Views/vInicio.xaml.cs
+++ Views/vInicio.xaml.cs
@@ -2,8 +2,16 @@ namespace BilleteraDigital.Views;
 
 public partial class vInicio : ContentPage
 {
-	public vInicio()
+	public vInicio( string correo)
 	{
 		InitializeComponent();
+		lblBienvenida.Text = correo;
 	}
+
+    private async void OnCerrarSesionClicked(object sender, EventArgs e)
+    {
+		await Navigation.PopToRootAsync();
+    }
+
+    
 }
--- Views/vRegistroUser.xaml.cs
+++ Views/vRegistroUser.xaml.cs
@@ -1,9 +1,51 @@
+using BilleteraDigital.Modelo;
+using BilleteraDigital.Utilitario;
+using SQLite;
+using System.Threading.Tasks;
+
 namespace BilleteraDigital.Views;
 
 public partial class vRegistroUser : ContentPage
 {
+	private SQLiteAsyncConnection _database;
 	public vRegistroUser()
 	{
 		InitializeComponent();
+		_database = new SQLiteAsyncConnection(Constante.DatabasePath, Constante.Flags);
+		_database.CreateTableAsync<Usuario>();
 	}
+	
+	private async void OnRegistrarClicked(object sender, EventArgs e)
+	{
+		var nuevoUsuario = new Usuario
+		{
+			Correo = txtCorreo.Text.Trim(),
+			Contrasena = txtPassword.Text,
+		};
+
+		if (String.IsNullOrEmpty(nuevoUsuario.Correo) || string.IsNullOrEmpty(nuevoUsuario.Contrasena))
+		{
+			await DisplayAlert("Error", "Correo y contrase requeridos", "OK");
+			return;
+		}
+
+		try
+		{
+			await _database.InsertAsync(nuevoUsuario);
+			await DisplayAlert("Exito", "Usuario registrado correctamente", "OK");
+			await Navigation.PopAsync();
+		}
+
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Error", $"No se pudo regsitrar: {ex.Message}", "OK");
+		}
+	}
+
+    private async void OnAtras(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new vLogin());
+    }
+
+ 
 }
