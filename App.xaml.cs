06ffe0d4035bf96d2fbe76f8530b677893a7087a
OverSNKPC <alejandro_0994@hotmail.com>
2025-05-23 10:43:08 -0500

    Api Noticias funcional v2

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new NavigationPage(new Views.vLogin()));
+            return new Window(new NavigationPage(new Views.NoticiasView()));
         }
     }
 }
